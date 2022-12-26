7e81dd7420868c5f6aac9aa234dad45e9dbee700
Timeshin <timeshin0485@gmail.com>
2022-12-26 19:44:00 +0300

    Configure environment Add Docker, husky, linter, prettier, packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+
 const nextConfig = {
-  reactStrictMode: true,
+  eslint: {
+    dirs: ['src']
+  },
+  typescript: {
+    tsconfigPath: './tsconfig.json'
+  },
+  reactStrictMode: false,
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	console.log('re-render APP', process.env.NODE_ENV_STATE)
+	return <Component {...pageProps} />
 }
